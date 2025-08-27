28b4e125c21bf9d681764fdb786fba457a76a82a
mariatsarouche <it2021100@hua.gr>
2025-08-27 13:49:13 +0300

    added .py file

---
+++ linear_regression.py
@@ -0,0 +1,16 @@
+
+
+class LinearRegression:
+
+    def fit(self, X, y):
+
+
+    def predict(self, X):
+
+
+    def evaluate(self, X, y):
+
+
+    def __init__(self):
+        self.w = None
+        self.b = None
