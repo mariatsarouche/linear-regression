9ce46d40c32d5c9f1d7eeebd5f9bf77936a93346
mariatsarouche <it2021100@hua.gr>
2025-08-27 14:58:16 +0300

    added functions for fit

--- linear_regression.py
+++ linear_regression.py
@@ -1,9 +1,30 @@
+import numpy as np
+
+
+def checkIfNpArray(array, name=None):
+     if not isinstance(array, np.ndarray):
+         raise ValueError(f"{name} isn't a numpy array.")
+
+
+def rowsMatch(inp, out):
+    if not inp.shape[0] == out.shape[0]:
+        raise ValueError(f"Arrays dont have the same amount of rows.")
+
+def check1D(y):
+    if y.ndim != 1:
+        raise ValueError(f"y must be 1D, it's {y.ndim}D")
+
+def verifyArray(X, y):
+    checkIfNpArray(X, "X")
+    checkIfNpArray(y, "y")
+    rowsMatch(X, y)
+    check1D(y)
 
 
 class LinearRegression:
 
     def fit(self, X, y):
-
+        verifyArray(X, y)
 
     def predict(self, X):
 
